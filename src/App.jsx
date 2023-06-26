b836719981abb7189ca8eda216c28ed32de00df0
Santiago Arias <sanargo97@gmail.com>
2023-06-26 15:22:49 -0500

    Ending the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,76 @@
 import Card from "./components/Card";
-
+import { IoIosCloudyNight } from "react-icons/io";
 import { useState } from "react";
 
 function App() {
+  //Dark mode
   const [darkMode, setDarkMode] = useState(false);
 
+  //Using the input value for the formulas
+  const [value, setValue] = useState("");
+  const [valueLength, setvalueLength] = useState("");
+  const [valueVolume, setvalueVolume] = useState("");
+  const [valueMass, setvalueMass] = useState("");
+
+  //Constant units for the formulas
+  const meterToFeet = 3.2808;
+  const literToGallon = 0.26417;
+  const kiloToPound = 2.204;
+
+  //The result of the input value
+  const handleInputValue = (e) => {
+    setValue(e.target.value);
+  };
+
+  //Formulas and the text for the <p> tag
+  const length = () => {
+    const meterFeet = value * meterToFeet;
+    const feetMeter = value / meterToFeet;
+    const result = `${value} meters = ${meterFeet.toFixed(
+      3
+    )} feet | ${value} feet = ${feetMeter.toFixed(3)} meters`;
+    return result;
+  };
+  const volume = () => {
+    let literGalon = value * literToGallon;
+    let galonLiter = value / literToGallon;
+    let result = `${value} liters = ${literGalon.toFixed(
+      3
+    )} gallons | ${value} gallons = ${galonLiter.toFixed(3)} liters`;
+    return result;
+  };
+  const mass = () => {
+    let kiloPound = value * kiloToPound;
+    let poundKilo = value / kiloToPound;
+    let result = `${value} kilos = ${kiloPound.toFixed(
+      3
+    )} pounds | ${value} pounds = ${poundKilo.toFixed(3)} kilos`;
+    return result;
+  };
+
+  //The action of the button
+  const handleBtn = () => {
+    setvalueLength(length());
+    setvalueVolume(volume());
+    setvalueMass(mass());
+  };
+
   return (
-    <div className="bg-slate-950 h-screen">
-      <Card darkMode={darkMode} />
+    <div className="bg-slate-950 h-screen ">
+      <header className="w-full flex justify-center py-5 mb-10 md:mb-32">
+        <IoIosCloudyNight
+          className="text-gray-50 text-5xl cursor-pointer"
+          onClick={() => setDarkMode(!darkMode)}
+        />
+      </header>
+      <Card
+        darkMode={darkMode}
+        inputValue={handleInputValue}
+        handleClick={handleBtn}
+        length={valueLength}
+        volume={valueVolume}
+        mass={valueMass}
+      />
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
-function Card({ darkMode }) {
+function Card({ darkMode, inputValue, handleClick, length, volume, mass }) {
   return (
-    <section className="w-80 bg-white font-inter md:w-96 ">
-      <div className="bg-indigo-600 flex flex-col items-center px-5 py-6">
+    <section className="w-80 bg-white font-inter mx-auto md:w-1/4">
+      <div className="bg-violet-600 flex flex-col items-center px-5 py-6">
         <h1 className="text-white font-bold text-xl text-center">
           Metric/Imperial Unit Conversion
         </h1>
@@ -9,17 +9,35 @@ function Card({ darkMode }) {
           type="number"
           name="unit"
           id="unit"
-          className="bg-transparent p-2 border border-white rounded-md w-20 h-14 my-5"
+          className="bg-transparent p-2 border border-gray-400 rounded-md w-24 h-16 my-5 text-white font-bold text-5xl text-center "
+          onChange={inputValue}
         />
-        <button className="bg-white p-2 w-fit h-8 rounded-md flex justify-center items-center">
+        <button
+          className="bg-white p-2 w-24 h-8 rounded-md flex justify-center items-center text-sm"
+          onClick={handleClick}
+        >
           Convert
         </button>
       </div>
       <div className={darkMode ? "dark" : ""}>
-        <div className="bg-gray-50 dark:bg-slate-900">
-          <div className="bg-white">
-            <h3></h3>
-            <p></p>
+        <div className="bg-gray-200 flex flex-col items-center py-8 gap-5 dark:bg-slate-900">
+          <div className="bg-white w-11/12 p-3 text-center dark:bg-slate-800">
+            <h3 className="text-indigo-950 font-bold my-2 dark:text-violet-300">
+              Length (Meter/Feet)
+            </h3>
+            <p className="text-sm text-gray-600 dark:text-gray-400">{length}</p>
+          </div>
+          <div className="bg-white w-11/12 p-3 text-center dark:bg-slate-800">
+            <h3 className="text-indigo-950 font-bold my-2 dark:text-violet-300">
+              Volume (Liters/Gallons)
+            </h3>
+            <p className="text-sm text-gray-600 dark:text-gray-400">{volume}</p>
+          </div>
+          <div className="bg-white w-11/12 p-3 text-center dark:bg-slate-800">
+            <h3 className="text-indigo-950 font-bold my-2 dark:text-violet-300">
+              Mass (Kilograms/Pounds)
+            </h3>
+            <p className="text-sm text-gray-600 dark:text-gray-400">{mass}</p>
           </div>
         </div>
       </div>
